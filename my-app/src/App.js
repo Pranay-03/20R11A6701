74b6de904900324057c4e5fff7684fb154408af2
Arigela Pranay kumar <arigelapranaykumar123@gmail.com>
2023-06-25 13:30:34 +0530

    Api images

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import moment from 'moment';
-import './App.css';
+import React from "react";
+import moment from "moment";
+import "./App.css";
 
 function App() {
   const trainSchedule = [
-    { trainNumber: '123', destination: 'City A', departureTime: '09:00 AM' },
-    { trainNumber: '456', destination: 'City B', departureTime: '10:30 AM' },
-    { trainNumber: '789', destination: 'City C', departureTime: '12:15 PM',no:'3' },
+    {
+      trainNumber: "123",
+      destination: "Chennai",
+      departureTime: "09:00 AM",
+      no: "1",
+    },
+    {
+      trainNumber: "456",
+      destination: "MUmbai",
+      departureTime: "10:30 AM",
+      no: "2",
+    },
+    {
+      trainNumber: "789",
+      destination: "Vapi",
+      departureTime: "12:15 PM",
+      no: "3",
+    },
   ];
 
   return (
@@ -27,7 +42,9 @@ function App() {
               <td>{train.trainNumber}</td>
               <td>{train.destination}</td>
               <td>{train.no}</td>
-              <td>{moment(train.departureTime, 'hh:mm A').format('hh:mm A')}</td>
+              <td>
+                {moment(train.departureTime, "hh:mm A").format("hh:mm A")}
+              </td>
             </tr>
           ))}
         </tbody>
